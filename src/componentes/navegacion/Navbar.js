23f408b8510453742e937c982bf78524760f2f53
Gabriel Poma <lgabrielpoma@gmail.com>
2023-07-08 12:13:50 +0200

    Cambio de colores en la barra de navegacion

--- src/componentes/navegacion/Navbar.js
+++ src/componentes/navegacion/Navbar.js
@@ -1,23 +1,10 @@
-import React /*{useEffect, useState}*/ from 'react';
+import React from 'react';
 import styles from './NavBar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
- /* const [hoverColor, setHoverColor] = useState('#b3c8fe');
-
-  useEffect(() =>{
-    const colors = ['#D03DFF', '#FF3DB8', '#FF3D3D', '#FFC73D', '#4FFF3D', '#3DE5FF', '#463DFF'];
-    let currentIndex = 0;
-
-    const intervalId = setInterval(() => {
-      currentIndex = (currentIndex + 1) % colors.length;
-      setHoverColor(colors[currentIndex]);
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
-*/
   return (
-    <nav className={styles.nav} /*style={{'--hover-color': hoverColor }}*/>
+    <nav className={styles.nav}>
       <ul>
         <li className="logo">
           <Link to="/Inicio">N-EX <span className={styles.mini}></span></Link>
@@ -30,13 +17,13 @@ const Navbar = () => {
         </li>
         <li>
           <Link to="/Productos">Productos</Link>
-          </li>
+        </li>
         <li>
-            <div>
-                <Link to="/Login">
-                    <img src='./Login.png' alt= "Login" width="25" height="25"/>
-                </Link>
-            </div>
+          <div>
+            <Link to="/Login">
+              <img src="./Login.png" alt="Login" width="25" height="25" />
+            </Link>
+          </div>
         </li>
       </ul>
     </nav>
